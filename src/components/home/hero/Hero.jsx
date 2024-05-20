30558f6bb09a15d68845589374f5291999669efd
youmen-Alswidan <youmenalswidan88@gmail.com>
2024-05-20 18:32:15 +0200

    make the buttons routable

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
-import "./hero.css"
-import Title from '../../common/title/Title'
-import prof from '../../../assets/prof_photo.jpg'
-
-
-
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import './hero.css';
+import Title from '../../common/title/Title';
+import prof from '../../../assets/prof_photo.jpg';
 
 const Hero = () => {
-  const [btn, setBtn] = useState("")
+  const [btn, setBtn] = useState("");
+  let history = useHistory();
+
+  const handleGetStartedClick = () => {
+    history.push('/login');
+  };
+  const handleViewCourses = () => {
+    history.push('/courses');
+  };
 
   return (
     <>
@@ -18,28 +24,20 @@ const Hero = () => {
               <img src={prof} alt="Description" />
             </div>
             <div className="div1">
-              < Title subtitle='welcome to master mind' title='the best education online' />
+              <Title subtitle='welcome to master mind' title='the best education online' />
               <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts</p>
-              <button onClick={() => setBtn("test1")} className="home_btn1">
-                GET STARTED NOW
+              <button onClick={handleGetStartedClick} className="home_btn1">
+                GET STARTED NOW !
               </button>
-              <button onClick={() => setBtn("test2")} className='home_btn2' >
+              <button onClick={handleViewCourses} className='home_btn2'>
                 VIEW COURSES <i className='fa fa-long-arrow-alt-right'></i>
               </button>
-
             </div>
-
-
           </div>
-
-
         </div>
       </section>
-      <div className="marigin">
-
-      </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
